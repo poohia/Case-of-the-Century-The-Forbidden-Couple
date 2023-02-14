14c41b6edc69562aec064ddf2a487abfed43f654
joazco <jazoulay@joazco.com>
2023-02-14 10:40:32 +0100

    avancement

--- src/GameDevSoftware/modules/retrospaceadventure/scenes/components/styled/Card.tsx
+++ src/GameDevSoftware/modules/retrospaceadventure/scenes/components/styled/Card.tsx
@@ -6,7 +6,10 @@ import { ContainerRowComponent } from "../RetrospacegameadventurefightsceneStyle
 import { useEffect, useState } from "react";
 import { TranslationComponent } from "../../../../../../components";
 
-export const CardContainer = styled.div<{ active?: boolean }>`
+export const CardContainer = styled.div<{
+  active?: boolean;
+  showEffects: boolean;
+}>`
   height: 90%;
   margin: 1%;
   padding: ${({ active }) => (active ? "2px" : "5px")};
@@ -20,6 +23,31 @@ export const CardContainer = styled.div<{ active?: boolean }>`
   justify-content: space-around;
   cursor: pointer;
   ${({ active }) => active && "border: 3px solid orange;"}
+  perspective: 150rem;
+  position: relative;
+  box-shadow: none;
+  background-color: white;
+  > div {
+    transition: all 0.8s ease;
+    backface-visibility: hidden;
+    position: absolute;
+    top: 0;
+    left: 0;
+    &:nth-child(1) {
+      // background-image: linear-gradient(160deg, #0093e9 0%, #80d0c7 100%);
+      ${({ showEffects }) => showEffects && " transform: rotateY(180deg);"}
+    }
+    &:nth-child(2) {
+      transform: rotateY(-180deg);
+      // background-image: linear-gradient(
+      //   43deg,
+      //   #4158d0 0%,
+      //   #c850c0 46%,
+      //   #ffcc70 100%
+      // );
+      ${({ showEffects }) => showEffects && " transform: rotateY(0deg);"}
+    }
+  }
 `;
 
 const CardContainerHeaderRow = styled(ContainerRowComponent)`
@@ -54,6 +82,7 @@ type CardProps = {
 
 const Card: React.FC<CardProps> = ({ card, active = false, onClick }) => {
   const [initView, setInitView] = useState<boolean>(true);
+  const [showEffects, setShowEffect] = useState<boolean>(false);
 
   useEffect(() => {
     setTimeout(() => setInitView(false), 1000);
@@ -66,32 +95,66 @@ const Card: React.FC<CardProps> = ({ card, active = false, onClick }) => {
         active ? "animate__animated animate__pulse" : ""
       }`}
       active={active}
+      showEffects={showEffects}
     >
-      <CardContainerHeaderRow>
-        <div>
-          <img src={card.image} alt="" />
-        </div>
-        <div>
-          <span>
-            <TranslationComponent id={card._title} />
-          </span>
-        </div>
-        <div>
-          <span>+{card.laser}</span>
-        </div>
-      </CardContainerHeaderRow>
-      <CardContainerRow style={{ flexDirection: "row" }}>
-        <TranslationComponent id="label_damage" />
-        <span>{card.damage}</span>
-      </CardContainerRow>
-      <CardContainerRow>
-        <TranslationComponent id="label_critical_effect" />
-        <TranslationComponent id={card.critical_effect.description} />
-      </CardContainerRow>
-      <CardContainerRow>
-        <TranslationComponent id="label_echec_effect" />
-        <TranslationComponent id={card.echec_effect.description} />
-      </CardContainerRow>
+      <div>
+        <CardContainerHeaderRow>
+          <div>
+            <img
+              src={card.image}
+              alt=""
+              onClick={(e) => {
+                e.preventDefault();
+                e.stopPropagation();
+                setShowEffect(!showEffects);
+              }}
+            />
+          </div>
+          <div>
+            <span>
+              <TranslationComponent id={card._title} />
+            </span>
+          </div>
+          <div>
+            <span>+{card.laser}</span>
+          </div>
+        </CardContainerHeaderRow>
+        <CardContainerRow style={{ flexDirection: "row" }}>
+          <TranslationComponent id="label_damage" />
+          <span>{card.damage}</span>
+        </CardContainerRow>
+      </div>
+      <div>
+        <CardContainerHeaderRow>
+          <div>
+            <img
+              src={card.image}
+              alt=""
+              onClick={(e) => {
+                e.preventDefault();
+                e.stopPropagation();
+                setShowEffect(!showEffects);
+              }}
+            />
+          </div>
+          <div>
+            <span>
+              <TranslationComponent id={card._title} />
+            </span>
+          </div>
+          <div>
+            <span>+{card.laser}</span>
+          </div>
+        </CardContainerHeaderRow>
+        <CardContainerRow>
+          <TranslationComponent id="label_critical_effect" />
+          <TranslationComponent id={card.critical_effect.description} />
+        </CardContainerRow>
+        <CardContainerRow>
+          <TranslationComponent id="label_echec_effect" />
+          <TranslationComponent id={card.echec_effect.description} />
+        </CardContainerRow>
+      </div>
     </CardContainer>
   );
 };
