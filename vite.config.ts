68fc4e13e968ec318a095254106d0aadd8acbae4
Jordan AZOULAY <jazoulay@joazco.com>
2025-01-22 14:41:58 +0100

    feature: migrate to yarn

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import commonjs from "vite-plugin-commonjs";
 import { viteRequire } from "vite-require";
 import dynamicImport from "vite-plugin-dynamic-import";
 
