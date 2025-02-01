f1667bcb35b3d70cb8ab1f073a792153cc0f34b5
Jordan AZOULAY <jazoulay@joazco.com>
2025-02-01 12:06:55 +0100

    feature:  screen orientation, capacitor.json done

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from "@capacitor/cli";
-
-const config: CapacitorConfig = {
-  appId: "com.gamedev.target",
-  appName: "Game Dev Target",
-  webDir: "build",
-};
-
-export default config;
