cbb934603bb44577cfb8ada783869bda2e784d3b
Jordan AZOULAY <jazoulay@joazco.com>
2025-09-28 15:11:00 +0200

    feature: add class sr-only

--- src/components/GlobalCSSComponent/index.tsx
+++ src/components/GlobalCSSComponent/index.tsx
@@ -106,6 +106,18 @@ const GlobalCSSComponent = createGlobalStyle<{
         return "";
     }
   }}
+  
+  /* screen reader only */
+  .sr-only {
+    position: absolute;
+    width: 1px;
+    height: 1px;
+    padding: 0;
+    margin: -1px;
+    overflow: hidden;
+    clip: rect(0, 0, 0, 0);
+    border: 0;
+  }
 
   ${({ platform }) =>
     platform === "browserandroid"
