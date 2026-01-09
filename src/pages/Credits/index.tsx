7ac8781501f04f352a2643679b8f306e30fb5efe
Jordan AZOULAY <jazoulay@joazco.com>
2026-01-09 15:43:11 +0100

    feature: credits done

--- src/pages/Credits/index.tsx
+++ src/pages/Credits/index.tsx
@@ -2,11 +2,26 @@ import { useGameProvider } from "../../gameProvider";
 import { PageComponent, TranslationComponent } from "../../components";
 
 const Credits = () => {
-  const { push } = useGameProvider();
+  const { push, getCredits } = useGameProvider();
   return (
     <PageComponent>
       <div>
         Credits
+        <div>
+          {getCredits().map((credit) => (
+            <div key={credit.title}>
+              <h2>{credit.title}</h2>
+              {credit.persons.map((person) => (
+                <div key={`${credit.title}-${person.name}`}>
+                  <h4>
+                    <span>{person.name}</span> -{" "}
+                    <TranslationComponent id={person.title} />
+                  </h4>
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
         <button onClick={() => push("home")}>
           <TranslationComponent id="label_back_home" />
         </button>
