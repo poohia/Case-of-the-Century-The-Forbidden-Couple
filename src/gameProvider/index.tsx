8005431120f097434d2372d215c741e6a8710597
Jordan AZOULAY <jazoulay@joazco.com>
2026-02-14 13:22:44 +0100

    feature: add html translation

--- src/gameProvider/index.tsx
+++ src/gameProvider/index.tsx
@@ -1,226 +1,151 @@
-import {
-  createContext,
-  useContext,
-  ReactNode,
-  useState,
-  useEffect,
-} from "react";
+import { useState, useCallback, useEffect, useMemo } from "react";
+import { Device } from "@capacitor/device";
 
-import { GlobalCSSComponent } from "../components";
-import { useStatusBarConfig } from "../hooks";
-import {
-  useTranslations,
-  GameProviderHooksInterface,
-  useRouter,
-  useEnv,
-  useSave,
-  useApplication,
-  useConstants,
-  useSound,
-  useSplashscreen,
-  useFonts,
-  useSmartAppBanner,
-  useScreenOrientation,
-  useAssets,
-  useVibrate,
-  useHolidaysOverlay,
-} from "./hooks";
-import useParameters from "./hooks/useParameters";
+import languages from "../../../GameDevSoftware/languages.json";
+import { GameProviderHooksDefaultInterface } from "..";
+import { ParametersType } from "../../../types";
 
-interface GameContextInterface extends GameProviderHooksInterface {}
+export interface useTranslationsInterface
+  extends
+    GameProviderHooksDefaultInterface,
+    ReturnType<typeof useTranslations> {}
 
-// export function createCtx<ContextType>() {
-//   const ctx = createContext<ContextType | undefined>(undefined);
-//   console.log("🚀 ~ ctx:", ctx);
-//   function useCtx() {
-//     const c = useContext(ctx);
-//     if (!c) {
-//       throw new Error("useCtx must be inside a Provider with a value");
-//     }
-//     return c;
-//   }
-//   return [useCtx, ctx.Provider] as const;
-// }
-
-// const [useGameProvider, CtxProvider] = createCtx<GameContextInterface>();
-
-// @ts-ignore
-const Ctx = createContext<GameContextInterface>({});
-const useGameProvider = () => {
-  return useContext(Ctx);
+export type TextTransformOptions = {
+  capitalize?: boolean;
+  toLowercase?: boolean;
+  toUppercase?: boolean;
 };
 
-type GameProviderProps = {
-  children: ReactNode;
-};
-
-const GameProvider = ({ children }: GameProviderProps) => {
+const useTranslations = (
+  parameters: ParametersType,
+  isMobileDevice: boolean,
+  setLocale: (locale: string) => void
+) => {
+  const [translations, setTranslations] = useState<
+    {
+      key: string;
+      text: string;
+      isHtml?: string;
+      textComputer?: string;
+      textMobile?: string;
+    }[]
+  >([]);
   const [loaded, setLoaded] = useState<boolean>(false);
-  useStatusBarConfig();
-  const {
-    loaded: loadedParameters,
-    parameters,
-    setLocale,
-    ...useParametersRest
-  } = useParameters();
-
-  const {
-    loaded: loadedRouter,
-    route,
-    params,
-    push,
-    pushNextScene,
-    ...useRouterRest
-  } = useRouter();
-  const { loaded: loadedEnv, env, demo, getEnvVar, ...useEnvRest } = useEnv();
-  const { loaded: loadedSave, ...useSaveRest } = useSave({
-    demo,
-    push,
-    pushNextScene,
-  });
-
-  const {
-    loaded: loadedSplashscreen,
-    SplashScreenComponent,
-    showSplashscreen,
-  } = useSplashscreen(getEnvVar);
-
-  const { loaded: loadedFonts, FontStyle, ...useFontsRest } = useFonts();
-
-  const {
-    loaded: loadedApplication,
-    appConfig,
-    background,
-    primaryFont,
-    platform,
-    isMobileDevice,
-    ...useApplicationRest
-  } = useApplication(loadedSplashscreen);
-  const { HolidaysOverlayComponent, ...useHolidaysOverlayRest } =
-    useHolidaysOverlay(getEnvVar);
-
-  const { loaded: loadedSmartAppBanner, SmartAppBanner } = useSmartAppBanner(
-    appConfig,
-    env,
-    platform,
-    getEnvVar
+  const defaultLocale = useMemo(
+    () => languages.find((language) => language.default) || languages[0],
+    []
   );
 
-  const { loaded: loadedScreenOrientation, ScreenOrientationForce } =
-    useScreenOrientation(appConfig, env, isMobileDevice, getEnvVar);
+  const loadLanguage = useCallback(async (language: string) => {
+    setTranslations(require(`../../../translations/${language}.json`).default);
+    return;
+  }, []);
 
-  const { getValueFromConstant, ...useConstatsRest } =
-    useConstants(isMobileDevice);
-
-  const { getAssetSound, ...useAssetsRest } = useAssets(
-    platform,
-    getValueFromConstant
+  const switchLanguage = useCallback(
+    (language: string) => {
+      setLocale(language);
+    },
+    [setLocale]
   );
 
-  const { loaded: loadedTranslations, ...useTranslationsRest } =
-    useTranslations(parameters, isMobileDevice, setLocale);
+  const formatCodeSameLanguage = useCallback((code: string): string => {
+    if (code.includes("id") || code.includes("in")) {
+      return "id";
+    }
+    if (code.includes("nb") || code.includes("nn") || code.includes("no")) {
+      return "no";
+    }
+    if (code.includes("ru") || code.includes("be")) {
+      return "ru";
+    }
+    if (code.includes("pt") || code.includes("pt-BR")) {
+      return "pt";
+    }
+    return code;
+  }, []);
 
-  const { loaded: loadedSound, ...useSoundRest } = useSound(
-    parameters.activatedMusic,
-    parameters.activatedSoundsEffect,
-    platform,
-    getAssetSound
-  );
-
-  const { ...useVibrateRest } = useVibrate(
-    platform,
-    parameters.activatedVibration
+  const translateText = useCallback(
+    (
+      key: string,
+      values: { key: string; value: string }[] = [],
+      defaultValue: string = key,
+      options?: TextTransformOptions
+    ) => {
+      const translationFind = translations.find(
+        (t) => t.key === key.replace("@t:", "")
+      );
+      let translationText = defaultValue;
+      if (translationFind) {
+        if (translationFind.textComputer && !isMobileDevice) {
+          translationText = translationFind.textComputer;
+        } else if (translationFind.textMobile && isMobileDevice) {
+          translationText = translationFind.textMobile;
+        } else {
+          translationText = translationFind.text;
+        }
+      }
+      values.forEach(
+        (value) =>
+          (translationText = translationText.replace(
+            `{${value.key}}`,
+            value.value
+          ))
+      );
+      if (translationText === key) {
+        console.warn(
+          `Translation: id '${key}' not found on language '${parameters.locale}'`
+        );
+      }
+      if (options?.capitalize) {
+        translationText =
+          translationText.charAt(0).toUpperCase() + translationText.slice(1);
+      } else if (options?.toLowercase) {
+        translationText = translationText.toLocaleLowerCase();
+      } else if (options?.toUppercase) {
+        translationText = translationText.toLocaleUpperCase();
+      }
+      return translationText;
+    },
+    [translations, isMobileDevice, parameters]
   );
 
   useEffect(() => {
-    if (
-      loadedParameters &&
-      loadedApplication &&
-      loadedTranslations &&
-      loadedRouter &&
-      loadedEnv &&
-      loadedSave &&
-      loadedSound &&
-      loadedSplashscreen &&
-      loadedFonts &&
-      loadedSmartAppBanner &&
-      loadedScreenOrientation
-    ) {
-      setLoaded(true);
-    }
-  }, [
-    env,
-    loadedParameters,
-    loadedApplication,
-    loadedTranslations,
-    loadedRouter,
-    loadedEnv,
-    loadedSave,
-    loadedSound,
-    loadedSplashscreen,
-    loadedFonts,
-    loadedSmartAppBanner,
-    loadedScreenOrientation,
-  ]);
+    if (parameters.locale) {
+      loadLanguage(parameters.locale).then(() => setLoaded(true));
+    } else {
+      Device.getLanguageCode()
+        .then(({ value }) => {
+          const languageFind =
+            languages.find((language) =>
+              formatCodeSameLanguage(value).includes(language.code)
+            ) || defaultLocale;
 
-  return (
-    <Ctx.Provider
-      value={{
-        ...useParametersRest,
-        ...useTranslationsRest,
-        ...useRouterRest,
-        ...useEnvRest,
-        ...useSaveRest,
-        ...useApplicationRest,
-        ...useHolidaysOverlayRest,
-        ...useConstatsRest,
-        ...useSoundRest,
-        ...useFontsRest,
-        ...useAssetsRest,
-        ...useVibrateRest,
-        appConfig,
-        parameters,
-        env,
-        demo,
-        loaded,
-        platform,
-        route,
-        params,
-        background,
-        primaryFont,
-        isMobileDevice,
-        setLocale,
-        push,
-        pushNextScene,
-        getEnvVar,
-        getValueFromConstant,
-        getAssetSound,
-      }}
-    >
-      <>
-        <ScreenOrientationForce />
-        <HolidaysOverlayComponent />
-        {loaded && <SmartAppBanner />}
-        <FontStyle />
-        <GlobalCSSComponent
-          background={background}
-          primaryFont={primaryFont}
-          platform={platform}
-          activatedDyslexia={parameters.activatedDyslexia}
-          sizeText={parameters.sizeText || "normal"}
-          colorMode={parameters.colorMode || "normal"}
-        />
-        {loaded ? (
-          children
-        ) : (
-          <SplashScreenComponent
-            onSplashscreenFinished={() => showSplashscreen(false)}
-          />
-        )}
-      </>
-    </Ctx.Provider>
+          setLocale(languageFind.code);
+          loadLanguage(languageFind.code).then(() => setLoaded(true));
+        })
+        .catch(() => {
+          loadLanguage(defaultLocale.code).then(() => setLoaded(true));
+        });
+    }
+  }, [parameters, loadLanguage, setLocale]);
+
+  const isTranslationHtml = useCallback(
+    (key: string) => {
+      const translationFind = translations.find(
+        (t) => t.key === key.replace("@t:", "")
+      );
+      return !!translationFind?.isHtml;
+    },
+    [translations]
   );
+
+  return {
+    translations,
+    loaded,
+    switchLanguage,
+    translateText,
+    isTranslationHtml,
+  };
 };
 
-export { useGameProvider };
-export default GameProvider;
+export default useTranslations;
