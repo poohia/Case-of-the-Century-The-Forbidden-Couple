dfb53cd1ad66dee02b8f6f7c87fb88dce3d6c1db
Jordan AZOULAY <jazoulay@joazco.com>
2026-02-18 11:06:00 +0100

    fix: fix assets

--- src/gameProvider/hooks/useAssets/index.ts
+++ src/gameProvider/hooks/useAssets/index.ts
@@ -1,7 +1,7 @@
 import { useCallback } from "react";
 
 import assets from "../../../GameDevSoftware/assets.json";
-import { Platform } from "../../../types";
+import { AssertAcceptedType, Platform } from "../../../types";
 import { GameProviderHooksDefaultInterface } from "..";
 
 export interface useAssetsInterface
@@ -11,6 +11,19 @@ const useAssets = (
   platform: Platform | null,
   getValueFromConstant: <T = any>(key: string) => T
 ) => {
+  const folderByType = useCallback((type: AssertAcceptedType): string => {
+    switch (type) {
+      case "image":
+        return "images/";
+      case "sound":
+        return "sounds/";
+      case "video":
+        return "videos/";
+      case "json":
+        return "";
+    }
+  }, []);
+
   const getAlt = useCallback((name: string) => {
     const findAsset = assets.find(
       (asset: { type: string; name: string }) =>
@@ -25,36 +38,40 @@ const useAssets = (
     return findAsset.alt || "";
   }, []);
 
-  const getAsset = useCallback((name: string): string | object => {
-    if (name.startsWith("assets/")) {
-      return name;
-    }
-    const findAsset = assets.find(
-      (asset: { type: string; name: string }) =>
-        asset.name === name.replace("@a:", "")
-    );
-
-    if (!findAsset) {
-      throw new Error(`Asset not found ${name.replace("@a:", "")}`);
-    }
-
-    if (findAsset.type === "json") {
-      return JSON.parse(
-        JSON.stringify(
-          require(
-            `../../../GameDevSoftware/configurationsFiles/${name.replace(
-              "@a:",
-              ""
-            )}`
-          ).default
-        )
+  const getAsset = useCallback(
+    (name: string): string | object => {
+      if (name.startsWith("assets/")) {
+        return name;
+      }
+      const findAsset = assets.find(
+        (asset: { type: string; name: string }) =>
+          asset.name === name.replace("@a:", "")
       );
-    }
 
-    const { name: nameAsset } = findAsset;
+      if (!findAsset) {
+        throw new Error(`Asset not found ${name.replace("@a:", "")}`);
+      }
 
-    return `assets/${findAsset.type}/${nameAsset}`;
-  }, []);
+      if (findAsset.type === "json") {
+        return JSON.parse(
+          JSON.stringify(
+            require(
+              `../../../GameDevSoftware/configurationsFiles/${name.replace(
+                "@a:",
+                ""
+              )}`
+            ).default
+          )
+        );
+      }
+
+      const typeAsset = folderByType(findAsset.type as AssertAcceptedType);
+      const { name: nameAsset } = findAsset;
+
+      return `assets/${typeAsset}${nameAsset}`;
+    },
+    [folderByType]
+  );
 
   const getAssetImg = useCallback(
     (name: string): string => {
@@ -89,6 +106,7 @@ const useAssets = (
     },
     [getAsset]
   );
+
   const getAssetFromConstant = useCallback(
     (key: string) => {
       const constantValue = getValueFromConstant(key);
